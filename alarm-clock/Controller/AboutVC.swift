c3ddae73384c56b887c21c729ea2e715c4308331
Rick Pearce <rpearce63@yahoo.com>
2019-06-15 17:25:37 -0500

    add About page

---
+++ alarm-clock/Controller/AboutVC.swift
@@ -0,0 +1,19 @@
+//
+//  AboutVC.swift
+//  alarm-clock
+//
+//  Created by Rick on 6/15/19.
+//  Copyright © 2019 Rick Pearce. All rights reserved.
+//
+
+import UIKit
+
+class AboutVC : UIViewController {
+    
+    
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
